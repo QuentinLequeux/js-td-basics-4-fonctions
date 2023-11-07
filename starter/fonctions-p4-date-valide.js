c782cad4dc91679f9b951901d26474e15e4cbd32
Quentin Lequeux <quentin.lequeux@student.hepl.be>
2023-11-07 22:16:23 +0100

    Devoir

--- starter/fonctions-p4-date-valide.js
+++ starter/fonctions-p4-date-valide.js
@@ -11,12 +11,40 @@ FONCTIONS - PRÉPA 4 : Date valide - version 2
 
 // Fonction pour vérifier si une année est bissextile
 
+const Bissextile = parseInt(prompt("Entrez une année :"));
+
+function isBissextile(annee){
+    if ((Bissextile % 4 === 0 && Bissextile % 100 !== 0) || Bissextile % 400 === 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+if (isBissextile(2023)) {
+    console.log(`${Bissextile} est une année bissextile.`);
+} else{
+    console.log(`${Bissextile} n'est pas une année bissextile.`);
+}
 
 // Fonction pour vérifier si une date est valide
 
+function isValid(number){
+    if (date >= 1 && date <= 31){
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Demander à l'utilisateur de saisir une date
 
+const date = parseInt(prompt("Entrez une date :"));
 
 // Utilisation de la fonction isValid pour vérifier la date
 
+if (isValid(10)) {
+    console.log(`${date} est une date valide.`);
+} else{
+    console.log(`${date} n'est pas une date valide.`);
+}
